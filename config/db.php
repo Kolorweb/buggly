7ec98f5b3bd4ee808ad82e59ee46063b8cf9eab3
Liam Gillman <liamgillman@gmail.com>
2015-05-12 17:36:16 +0100

    Fixed db config syntax error

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     // 'dsn' => 'mysql:host=localhost;port=8889;dbname=buggly;unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock',
-    'dsn' => 'mysql:host=localhost;dbname=buggly;
+    'dsn' => 'mysql:host=localhost;dbname=buggly',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
