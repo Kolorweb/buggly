f1686e4b421a66f51fb56e458692596ee3cd02d9
Liam Gillman <liamgillman@gmail.com>
2015-05-14 11:56:22 +0100

    Db permissions fix

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    // 'class' => 'yii\db\Connection',
+    'class' => 'yii\db\Connection',
     // 'dsn' => 'mysql:host=localhost;port=8889;dbname=buggly;unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock',
     // 'username' => 'root',
     // 'password' => 'root',
