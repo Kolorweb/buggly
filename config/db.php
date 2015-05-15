32d8344ad2fed803e034340749b9460722ddd6c5
Liam Gillman <liamgillman@gmail.com>
2015-05-15 23:01:09 +0100

    Config db change to live

--- config/db.php
+++ config/db.php
@@ -2,15 +2,15 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;port=8889;dbname=buggly;unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock',
-    'username' => 'root',
-    'password' => 'root',
+    // 'dsn' => 'mysql:host=localhost;port=8889;dbname=buggly;unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock',
+    // 'username' => 'root',
+    // 'password' => 'root',
 
     /* Remove comments for deployment. */
 
-    // 'dsn' => 'mysql:host=localhost;dbname=buggly',
-    // 'username' => 'liamgillman',
-    // 'password' => 'Pepperoni123',
+    'dsn' => 'mysql:host=localhost;dbname=buggly',
+    'username' => 'liamgillman',
+    'password' => 'Pepperoni123',
 
     'charset' => 'utf8',
 ];
